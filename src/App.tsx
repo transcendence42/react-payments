eb91a91987715445186b0c5eed6495891b844627
hochan Lee <hochan049@gmail.com>
2021-06-15 11:00:20 +0900

    fix: test err

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          another React
+          Learn React
         </a>
       </header>
     </div>
