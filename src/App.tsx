4f3d5738ed2e47ab0d408e0c1b290c86dbef39d4
hochan Lee <hochan049@gmail.com>
2021-06-15 11:06:22 +0900

    fix: jest err

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learna React
+          Learn React
         </a>
       </header>
     </div>
