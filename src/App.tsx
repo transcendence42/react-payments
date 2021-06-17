1ab66a705c9bbbdf88abb878f4fbdeeeef320a87
hochan Lee <hochan049@gmail.com>
2021-06-17 14:16:58 +0900

    style: MainLayout__Wrapper 구현

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,18 @@ import CardForm from './Route/CardForm';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path={[ROUTE.HOME, ROUTE.CARD_LIST]}>
-            <CardList />
-          </Route>
-          <Route path={ROUTE.CARD_FORM}>
-            <CardForm />
-          </Route>
-        </Switch>
-      </Router>
+      <div className="MainLayout__Wrapper">
+        <Router>
+          <Switch>
+            <Route exact path={[ROUTE.HOME, ROUTE.CARD_LIST]}>
+              <CardList />
+            </Route>
+            <Route path={ROUTE.CARD_FORM}>
+              <CardForm />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 };
