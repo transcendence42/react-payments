756c4a54e642db4fc64ce92b86df6f10eff661e4
hochan Lee <hochan049@gmail.com>
2021-06-17 21:48:37 +0900

    feat: 버튼 기능 구현

---
+++ src/Components/Button/Button.stories.tsx
@@ -0,0 +1,4 @@
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+
+storiesOf('Button', module).addDecorator(withKnobs);
--- src/Route/CardForm/CardFormPresenter.tsx
+++ src/Route/CardForm/CardFormPresenter.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Card from '../../Components/Card';
+import Button from '../../Components/Button';
+import './CardForm.css';
 
 const CardFormPresenter = () => {
   return (
-    <div className="CardForm">
-      <div className="Title">
-        <button className="Button Button--backward">{'<'}</button>
-        <h2 className="Title__text">카드 추가</h2>
+    <div className="card-form">
+      <div className="card-form__header">
+        <button className="card-form__header-button">{'<'}</button>
+        <span className="card-form__header-title">카드 추가</span>
       </div>
-      <Card></Card>
+      <div className="card-form__body">
+        <Card></Card>
+      </div>
+      <div className="card-form__footer">
+        <Button className="button-form" type="submit">
+          제출 완료
+        </Button>
+      </div>
     </div>
   );
 };
