ee7606006029518e27efd7be1f5db7a77691df13
hochan Lee <hochan049@gmail.com>
2021-06-19 14:10:19 +0900

    feat: cardForm 디자인 완료

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,8 +1,8 @@
-// import React, { forwardRef } from 'react';
-// import styles from './Input.module.scss';
-// import classNames from 'classnames/bind';
+import React, { forwardRef } from 'react';
+import styles from './Input.module.scss';
+import classNames from 'classnames/bind';
 
-// const cx = classNames.bind(styles);
+const cx = classNames.bind(styles);
 
 export interface InputProps {
   className?: string | undefined;
@@ -10,13 +10,17 @@ export interface InputProps {
   width?: string | undefined;
 }
 
-// export const Input = forwardRef(({ className = '', placeholder = '', width = '' }: InputProps, ref: any) => {
-//   return (
-//     <input
-//       className={cx('input-container', className)}
-//       placeholder={placeholder}
-//       style={{ width: `${width}` }}
-//       ref={ref}
-//     />
-//   );
-// });
+const Input = forwardRef(({ className = '', placeholder = '', width = '' }: InputProps, ref: any) => {
+  return (
+    <input
+      className={cx('input-container', className)}
+      placeholder={placeholder}
+      style={{ width: `${width}` }}
+      ref={ref}
+    />
+  );
+});
+
+Input.displayName = 'holee-input';
+
+export { Input };
--- src/Route/CardForm/CardFormPresenter.tsx
+++ src/Route/CardForm/CardFormPresenter.tsx
@@ -1,79 +1,84 @@
 import React from 'react';
-import { Card, Button } from '../../Components';
-// import { Card, Button, Input, InputForm } from '../../Components';
+import { Card, Button, Input, InputForm } from '../../Components';
 import './CardForm.scss';
 
-// const CardFormBodyNumber = () => {
-//   return (
-//     <InputForm
-//       inputContainerClassName="card-form__body-number-container"
-//       inputFormTitle="카드번호"
-//       inputFormErrorMessage="error message"
-//     >
-//       <Input className="card-form__body-number-input" />
-//       <span className="card-form__body-number-seperator">-</span>
-//       <Input className="card-form__body-number-input" />
-//       <span className="card-form__body-number-seperator">-</span>
-//       <Input className="card-form__body-number-input" />
-//       <span className="card-form__body-number-seperator">-</span>
-//       <Input className="card-form__body-number-input" />
-//     </InputForm>
-//   );
-// };
+const CardFormBodyNumber = () => {
+  return (
+    <InputForm
+      inputContainerClassName="card-form__body-number-container"
+      inputFormTitle="카드번호"
+      inputFormErrorMessage="error message"
+    >
+      <Input className="card-form__body-number-input" placeholder="_ _ _ _" />
+      <span className="card-form__body-number-seperator">-</span>
+      <Input className="card-form__body-number-input" placeholder="_ _ _ _" />
+      <span className="card-form__body-number-seperator">-</span>
+      <Input className="card-form__body-number-input" placeholder="_ _ _ _" />
+      <span className="card-form__body-number-seperator">-</span>
+      <Input className="card-form__body-number-input" placeholder="_ _ _ _" />
+    </InputForm>
+  );
+};
 
-// const CardFormExpirationDate = () => {
-//   return (
-//     <InputForm
-//       inputContainerClassName="card-form__body-expiration-date-container"
-//       inputFormTitle="만료일"
-//       inputFormErrorMessage="error message"
-//       containerWidth="12rem"
-//     >
-//       <Input className="card-form__body-expiration-date-input" width="4rem" />
-//       <span className="card-form__body-expiration-date-seperator">/</span>
-//       <Input className="card-form__body-expiration-date-input" width="4rem" />
-//     </InputForm>
-//   );
-// };
+const CardFormExpirationDate = () => {
+  return (
+    <InputForm
+      inputContainerClassName="card-form__body-expiration-date-container"
+      inputFormTitle="만료일"
+      inputFormErrorMessage="error message"
+      containerWidth="5.6rem"
+    >
+      <Input className="card-form__body-expiration-date-input" width="1.5rem" placeholder="MM" />
+      <span className="card-form__body-expiration-date-seperator">/</span>
+      <Input className="card-form__body-expiration-date-input" width="1.5rem" placeholder="YY" />
+    </InputForm>
+  );
+};
 
-// const CardFormOwnerName = () => {
-//   return (
-//     <InputForm
-//       inputContainerClassName="card-form__body-owner-name-container"
-//       inputFormTitle="카드 소유자 이름(선택)"
-//       inputFormErrorMessage="error message"
-//     >
-//       <Input className="card-form__body-owner-name-input" placeholder="카드에 표시된 이름과 동일하게 입력하세요." />
-//     </InputForm>
-//   );
-// };
+const CardFormOwnerName = () => {
+  return (
+    <InputForm
+      inputContainerClassName="card-form__body-owner-name-container"
+      inputFormTitle="카드 소유자 이름(선택)"
+      inputFormErrorMessage="error message"
+    >
+      <Input
+        className="card-form__body-owner-name-input"
+        placeholder="카드에 표시된 이름과 동일하게 입력하세요."
+        width="19rem"
+      />
+    </InputForm>
+  );
+};
 
-// const CardFormCVC = () => {
-//   return (
-//     <InputForm
-//       inputContainerClassName="card-form__body-cvc-container"
-//       inputFormTitle="보안 코드(CVC/CVV)"
-//       inputFormErrorMessage="error message"
-//     >
-//       <Input className="card-form__body-cvc-input" />
-//     </InputForm>
-//   );
-// };
+const CardFormCVC = () => {
+  return (
+    <InputForm
+      inputContainerClassName="card-form__body-cvc-container"
+      inputFormTitle="보안 코드(CVC/CVV)"
+      inputFormErrorMessage="error message"
+      containerWidth="3.2rem"
+    >
+      <Input className="card-form__body-cvc-input" placeholder="CVC" width="2rem" />
+    </InputForm>
+  );
+};
 
-// const CardFormPassword = () => {
-//   return (
-//     <InputForm
-//       inputContainerClassName="card-form__body-password-container"
-//       inputFormTitle="카드 비밀번호"
-//       inputFormErrorMessage="error message"
-//     >
-//       {/* <Input className="card-form__body-password-input" />
-//       <Input className="card-form__body-password-input" />
-//       <Input className="card-form__body-password-input" />
-//       <Input className="card-form__body-password-input" /> */}
-//     </InputForm>
-//   );
-// };
+const CardFormPassword = () => {
+  return (
+    <InputForm
+      inputContainerClassName="card-form__body-password-container"
+      inputFormTitle="카드 비밀번호"
+      inputFormErrorMessage="error message"
+      containerWidth="8.5rem"
+    >
+      <Input className="card-form__body-password-input" placeholder="_" width="1rem" />
+      <Input className="card-form__body-password-input" placeholder="_" width="1rem" />
+      <Input className="card-form__body-password-input" placeholder="_" width="1rem" />
+      <Input className="card-form__body-password-input" placeholder="_" width="1rem" />
+    </InputForm>
+  );
+};
 
 const CardFormPresenter = () => {
   return (
@@ -86,11 +91,11 @@ const CardFormPresenter = () => {
       </div>
       <div className="card-form__body">
         <Card></Card>
-        {/* <CardFormBodyNumber />
+        <CardFormBodyNumber />
         <CardFormExpirationDate />
         <CardFormOwnerName />
         <CardFormCVC />
-        <CardFormPassword /> */}
+        <CardFormPassword />
       </div>
       <div className="card-form__footer">
         <Button className="card-form__footer-button" type="submit">
